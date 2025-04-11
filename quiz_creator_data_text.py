fd573b5d26131b0516c23a7650852d67701d1b57
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-11 22:36:41 +0800

    created a function to get the valid answer for the questions from the user

--- quiz_creator_data_text.py
+++ quiz_creator_data_text.py
@@ -1,3 +1,13 @@
 # import modules
 import json 
 import os 
+
+# Create a function to get a valid answer.
+def get_valid_answer(prompt):
+    # Ask the user for the answer (a, b, c, or d) and make sure it's valid.
+    while True:
+        answer = input(f"{prompt} (a, b, c, d): ").lower() 
+        if answer in ['a', 'b', 'c', 'd']: 
+            return answer 
+        else: 
+            print("Invalid input. Please enter 'a', 'b', 'c', or 'd' ONLY.") 
