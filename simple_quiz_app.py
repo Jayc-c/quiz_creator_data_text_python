6229933dea9e63f500d77534fce0575e6210b4cf
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-06-20 13:48:34 +0800

    open and load json data

--- simple_quiz_app.py
+++ simple_quiz_app.py
@@ -7,3 +7,8 @@ import random
 
 # created a function to load the quiz questions from the JSON file.
 def load_quiz_from_file(filename):
+    try:
+        # open and load JSON data.
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
