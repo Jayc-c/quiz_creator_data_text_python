17cfd28fdb9c17c261a693e584f3094cf5febf2f
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-06-20 13:45:36 +0800

    load quiz questions from json

---
+++ simple_quiz_app.py
@@ -0,0 +1,9 @@
+# import necessary modules.
+import tkinter as tk
+from tkinter import messagebox 
+import json 
+import time 
+import random 
+
+# created a function to load the quiz questions from the JSON file.
+def load_quiz_from_file(filename):
